c3a2547b2c5de060be182f3e354f54659ebd570b
André Escarião <andre.escariao1@gmail.com>
2022-05-12 22:07:03 -0300

    Add files via upload

---
+++ LIDAR_Sketch_Principal.ino
@@ -0,0 +1,15 @@
+/*
+Sketch Principal
+Controlador PID de Malha fechada utilizando ponte h, dois motores DC e dois Encoders Ópticos
+Objetivo: Fazer o robô andar 10cm e fazer uma curva utilizando o controlador PID
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
